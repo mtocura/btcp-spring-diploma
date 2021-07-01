379aaef7827d5f2e8fe6c03f6687843df0c2ce2b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 12:40:18 -0300

    Inicialização do projeto spring

---
+++ src/test/java/br/com/aula2/tt/diploma/DiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula2.tt.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
